b974b36b3b3cb48b34c62061972e4169c4be242c
Bruna Weber <brunadaniweber@hotmail.com>
2021-06-11 09:33:45 +0100

    Changes Maked in Guessing game and Word Search:
    - modified:   NumberGuessingGame09-06.py
    - modified:   WordSearch09-06.py

--- NumberGuessingGame09-06.py
+++ NumberGuessingGame09-06.py
@@ -1,17 +1,20 @@
 import random
 
-number = random.randint(1, 2) #the right number
+number = random.randint(1, 101) #the right number
 guess = 1
 while guess != number:
-    guess = input("Guess a number between 1 and 100: ")
+    guess = int(input("Guess a number between 1 and 100: "))
 
 
     if guess == number:
-        print("congratulations! You won! It took you {} tries.".count(guess))
-        break
+        print(f"congratulations! The number you entered is:{guess}")#Add it 11/06
+        break 
     elif guess > 100:
         print("type a number between 1 and 100!")
+        continue #add continue 11/06
     elif guess < number:
         print("Your guess is too low. Try again!")
+        continue
     else:
         print("Your guess is too high")
+        continue
--- WordSearch09-06.py
+++ WordSearch09-06.py
@@ -1,4 +1,22 @@
-file = open('C:/Users/bruna/Desktop/PythonExercices/PythonText.txt')
-txt = file.read()
+file = open('C:/Users/bruna/source/repos/PythonExercices/PythonText.txt', 'r')#open file
 
-searchTerm = ""
+text = file.read()#read a file
+
+text_list = text.split()#make the text as a list
+
+print(text_list)#print list of words just to test
+
+
+def wordSearch (text_list,searchTerm):
+    
+    searchTerm = input("Enter a word that you would like to Search: ")
+    for searchTerm in text_list:
+        if searchTerm in text_list:
+            searchTerm += 1
+            print(searchTerm)
+        else :
+            print("This text does not contain this word.Try search another one.")   
+
+
+
+#print(index = file.find("python"))
