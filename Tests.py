fb845bb7d2def18655ab37c644824c466e8cd173
Bruna Weber <brunadaniweber@hotmail.com>
2021-06-23 09:24:02 +0100

    Tests.py Word.Counter.py Files modified

---
+++ Tests.py
@@ -0,0 +1,21 @@
+#print("Hello " + input("what is your name?"))
+
+#String Format
+#person = {'name': 'Eric', 'age': 74}
+#print(("Hello, {name}. You are {age}.").format(name=person['name'], age=person['age']))
+###########################################
+#Working with Dictionaries:
+#Empty Dictionarie:
+##dict = {}
+#New Key and set its value in the dictionary
+#dict["Bruna"] = 32
+#print("Bruna" in dict.keys()) # true
+#print("Dani" in dict.keys()) # false
+
+###########################################
+
+#Dictionaries:
+
+dict = {"125JA": {"Title" : "TitleValue","Author": "AuthorValue", "PublicationYear": "YearValue"}}
+
+
